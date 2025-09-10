37d2e3b2b3ca5aff9351d4bab7fb865bf47bf59a
Belbut <80793911+Belbut@users.noreply.github.com>
2025-09-10 14:02:55 +0100

    chore: create dropdown list reusing toggle controller

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,17 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
   static classes = ["change"]
-  static targets = ["title", "btn"]
+  static targets = ["element", "btn"]
 
   connect() {
-    console.log(this.titleTargets)
-    // this.titleTarget.classList.toggle(this.changeClass)
-    this.update()
+    // console.log(this.elementTargets)
+    // // this.elementTarget.classList.toggle(this.changeClass)
+    // this.update() // start being displayed
   }
 
   update() {
     console.log("should toggle class here")
-    this.titleTargets.forEach(element => {
+    this.elementTargets.forEach(element => {
       element.classList.toggle(this.changeClass)
     });
   }
